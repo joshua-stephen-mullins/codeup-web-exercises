e649cff40d395e5f1a39ae43e0e644dc164955b2
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-06 16:57:57 -0500

    continuing class work

---
+++ extra-exercises/code-wars/pangram/js/pangram.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function isPangram(string){
+  let stringLower = string.toLowerCase();
+  return stringLower.includes(('a') && ('b') && ('c') && ('d') && ('e') && ('f') && ('g') && ('h') && ('i') && ('j') && ('k') && ('l') && ('m') && ('n') && ('o') && ('p') && ('q') && ('r') && ('s') && ('t') && ('u') && ('v') && ('w') && ('x') && ('y') && ('z'));
+}
+
+abcdefghijklmopqrstuvwxyz
