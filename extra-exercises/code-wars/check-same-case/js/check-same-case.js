05f5d0f4d98d30ba5893a8ce3c1c91f87acd95c4
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-08 18:56:19 -0500

    started new codewars problem

---
+++ extra-exercises/code-wars/check-same-case/js/check-same-case.js
@@ -0,0 +1,44 @@
+"use strict";
+
+(function(){
+
+// checking to make sure i know how to check the cases
+//     function sameCase(a, b){
+//         let aCheck = (a.toUpperCase() === a) + (b.toUpperCase() === b);
+//         return aCheck;
+//     }
+//
+//     console.log(sameCase('a', 'b'));
+//     console.log(sameCase('A', 'A'));
+
+
+    // i think if then statements will work best... start with the non characters and then others.
+
+    // function isLetter(c) {
+    //     return c.toLowerCase() != c.toUpperCase();
+    // }
+    function sameCase(a, b){
+        if (a.toLowerCase() == a.toUpperCase() || (b.toLowerCase() == b.toUpperCase())) {
+            return -1;
+        } else if ()
+
+    }
+
+    // console.log(sameCase('a', '$'));
+    console.log(sameCase("$", 'a'));
+    console.log(sameCase("a", 'b'));
+    // console.log(sameCase('d', 'e'));
+    // console.log(sameCase('z' , 'a'));
+
+    // failed checking if character is letter...
+    // function sameCase(a, b){
+    //     let alphabet = "a" || 'b' || 'c' || 'd' || "e" || 'f' || 'g' || 'h' || "i" || 'j' || 'k' || 'k' || "m" || 'n' || 'o' || 'p' || "q" || 'r' || 's' || 't' || "u" || 'v' || 'w' || 'x' || 'y' || 'z';
+    //     if (a || b !== alphabet) {
+    //         return 'Incorrect input';
+    //     }
+    //
+    // }
+    //
+    // console.log(sameCase('z' , 'a'));
+
+}())
