d5e39a19f24de205422a263954910b6116685278
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-11 09:31:40 -0500

    completed class warm up exercise

--- extra-exercises/extra-functions.js
+++ extra-exercises/extra-functions.js
@@ -4,39 +4,39 @@
 // ================ !! Mini-exercises !!
 
 // Write a function, returnFive, that returns the number five. No inputs should be defined.
-
-function returnFive() {
-    return 5;
-}
-
-console.log(returnFive());
+//
+// function returnFive() {
+//     return 5;
+// }
+//
+// console.log(returnFive());
 
 // Write a function, isFive, that takes in an input and returns the boolean value true if the passed argument is the number 5 or the string "5". Return false otherwise.
 // how many inputs should the function have?
 // what are the data types for the inputs?
 // what is the data type of the output/return value?
-
-function isFive(number) {
-    return number == 5;
-}
-
-console.log(isFive(5));
-console.log(isFive("5"));
-console.log(isFive("doggie"));
+//
+// function isFive(number) {
+//     return number == 5;
+// }
+//
+// console.log(isFive(5));
+// console.log(isFive("5"));
+// console.log(isFive("doggie"));
 
 // Write a function, isShortWord, that takes in a string and returns the boolean value true if the passed argument is shorter than 5 characters. Return false otherwise.
 
 // how many inputs?
 // what data type for inputs?
 // what is the return type?
-
-function isShortWord(word) {
-    return word.length < 5;
-}
-
-console.log(isShortWord("pig"));
-console.log(isShortWord("pizza"));
-console.log(isShortWord("very saucy"));
+//
+// function isShortWord(word) {
+//     return word.length < 5;
+// }
+//
+// console.log(isShortWord("pig"));
+// console.log(isShortWord("pizza"));
+// console.log(isShortWord("very saucy"));
 
 // const isShortWord = (word) => word.length < 5
 
@@ -44,11 +44,25 @@ console.log(isShortWord("very saucy"));
 // How many inputs?
 // What type of input?
 // What type of output?
+//
+// function isSameLength(wordLength1, wordLength2) {
+//     return wordLength1.length === wordLength2.length;
+// }
 
-function isSameLength(wordLength1, wordLength2) {
-    return wordLength1.length === wordLength2.length;
+
+
+function multiplyNumber(inputNumber){
+    if (typeof inputNumber !== 'number') {
+        return 0;
+    } else {
+        return inputNumber * 5;
+    }
 }
 
+console.log(multiplyNumber(2134));
+console.log(multiplyNumber("doggo"));
+console.log(multiplyNumber("5"));
+console.log(multiplyNumber("true"));
 }())
 
 
