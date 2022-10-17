ca42f6db26e4e2d988b14893659e8c265b5bd709
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-17 15:16:42 -0500

    completed review of array manipulation lesson

--- js/planets-string.js
+++ js/planets-string.js
@@ -11,7 +11,7 @@
      * console.log planetsArray to check your work
      */
 
-    planetsArray = planetsString.split(' ');
+    planetsArray = planetsString.split('|');
     console.log(planetsArray);
 
     /**
