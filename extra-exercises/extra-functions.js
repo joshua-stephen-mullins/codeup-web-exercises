67bbed42447eb7afa82e89b1b803dba0b2a05b4f
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-10-20 09:49:10 -0500

    completed morning warmup 20 oct

--- extra-exercises/extra-functions.js
+++ extra-exercises/extra-functions.js
@@ -100,16 +100,39 @@
 //         console.log(num);
 //     }
 
-function typePrinter(arr1){
-    let i = 0;
-    while (i < arr1.length){
-        let arrType = typeof arr1[i];
-        console.log(arrType);
-        i++;
-    }
+// function typePrinter(arr1){
+//     let i = 0;
+//     while (i < arr1.length){
+//         let arrType = typeof arr1[i];
+//         console.log(arrType);
+//         i++;
+//     }
+// }
+//
+// typePrinter([true, "Icon", 25, "66", false, 0]);
+
+
+    // Warm-Up:
+    //
+    // Create a function named "average" that takes in an array of numeric values, and returns the average. If any of the elements in the array are non-numeric, the function should return false.
+    //
+
+
+function average(array) {
+    let addTotal = 0;
+        for (let i = 0; i < array.length; i++) {
+            if (!isNaN(parseFloat(array))) {
+            addTotal += parseFloat(array[i]);
+                } return addTotal / array.length;
 }
+}
+    //     Ex:
 
-typePrinter([true, "Icon", 25, "66", false, 0]);
-
+    console.log(average(["6", 5, 3, 2, 9]))                       // returns 5
+    console.log(average([true, 6, 9, 3, 10]))                 // returns false
+    console.log(average([{name: "Codey the Duck"}, 10, 3, false])) // returns false
+    console.log(average([1, 2, 3, 4, 5]))                       // returns 3
+    console.log(average(["5.5", "five", 3, 6, 7]))              // returns false
+    console.log(average(["five", "5.5", 3, 6, 7]))              // returns false
 
 }());
