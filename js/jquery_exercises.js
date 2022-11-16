7ce302b12d5c6ed93cca656c2a7fb839b65de19a
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-11-16 12:04:55 -0600

    completed jquery events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -9,10 +9,27 @@
 
         // $('.codeup').css('border', '1px solid red')
 
-        $('li').css('font-size', '20px');
-        $('h1, p, li').css('background-color', 'yellow')
-        let h1Contents = $('h1').html();
-        alert(h1Contents);
+        // $('li').css('font-size', '20px');
+        // $('h1, p, li').css('background-color', 'yellow')
+        // let h1Contents = $('h1').html();
+        // alert(h1Contents);
 
+        $('h1').click(function(){
+            $('h1').css('background-color', 'yellow');
+        })
+
+
+        $('p').dblclick(function(){
+            $('p').css('font-size', '18px');
+        })
+
+        $('li').hover(
+            function(){
+                $('li').css('color', 'red')
+            },
+            function(){
+                $('li').css('color', 'black')
+        }
+        )
     });
 })();
