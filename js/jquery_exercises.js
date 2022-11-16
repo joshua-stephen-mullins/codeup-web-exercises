25611c32e589d56641b6765006d51a055b488ff9
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-11-16 10:11:08 -0600

    completed jquery selectors exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,4 +1,18 @@
 "use strict";
-$(function () {
-    alert('The DOM has finished loading!');
-});
+
+(function () {
+    $(document).ready(function () {
+        //jquery goes here
+        // let firstLineItemContents = $('#firstLineItem').html();
+        // console.log(firstLineItemContents);
+        // alert(firstLineItemContents);
+
+        // $('.codeup').css('border', '1px solid red')
+
+        $('li').css('font-size', '20px');
+        $('h1, p, li').css('background-color', 'yellow')
+        let h1Contents = $('h1').html();
+        alert(h1Contents);
+
+    });
+})();
