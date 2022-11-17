4f8630599ed9a654d20825d5e96d867b6540c128
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-11-17 16:54:22 -0600

    completed jquery traversing exercise minus bonus

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -11,4 +11,30 @@ $(document).ready(function(){
         $(this).toggleClass('highlighted');
 
     })
+
+    $('#lastItemButton').click(function(){
+        $('ul').each(function(){
+            $(this).children().last().css('background-color', 'yellow');
+    })
+    })
+
+    $('#evergladesTitle').click(function(){
+        $('#evergladesList').children().css('font-weight', 'bold');
+    })
+    $('#zionTitle').click(function(){
+        $('#zionList').children().css('font-weight', 'bold');
+    })
+    $('#bigBendTitle').click(function(){
+        $('#bigBendList').children().css('font-weight', 'bold');
+    })
+
+    $('.evergladesItem').click(function(){
+        $('#evergladesList').children().first().css('background-color', 'blue');
+    })
+    $('.zionItem').click(function(){
+        $('#zionList').children().first().css('background-color', 'blue');
+    })
+    $('.bigBendItem').click(function(){
+        $('#bigBendList').children().first().css('background-color', 'blue');
+    })
 });
